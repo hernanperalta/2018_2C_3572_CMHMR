5f3df6fea5823c38e05b3f5f2a3bf1df2ef7f490
Lucas <lucasleonardo1996@hotmail.com>
2018-10-22 17:54:51 -0300

    Corrijo la posicion inicial del personaje a como estaba originalmente

--- TGC.Group/Model/Personaje.cs
+++ TGC.Group/Model/Personaje.cs
@@ -53,7 +53,7 @@ namespace TGC.Group.Model
             //Configurar animacion inicial
             Mesh.playAnimation("Parado", true);
             //Escalarlo porque es muy grande
-            Mesh.Position = new TGCVector3(0, 0, -350); // 0,0,50
+            Mesh.Position = new TGCVector3(0, 0, 50); // 0,0,50
             Mesh.Scale = new TGCVector3(0.1f, 0.1f, 0.1f);
             ultimaPosicion = TGCMatrix.Translation(Mesh.Position);
         }
