e723df6b3c08328ffd0679623cc19b16fe3136a5
ltrinidad <lucasleonardo1996@hotmail.com>
2018-10-23 15:56:54 -0300

    ya estan los escalones

--- TGC.Group/Model/EscenarioPiramide.cs
+++ TGC.Group/Model/EscenarioPiramide.cs
@@ -60,17 +60,51 @@ namespace TGC.Group.Model
 
             escalones.Add(new Escalon(frente, arriba));
 
+            // Segundo escalon
             var frente2 = frente.createMeshInstance("frente2");
-            frente2.AutoTransform = false;
+            
             var arriba2 = arriba.createMeshInstance("arriba2");
-            arriba2.AutoTransform = false;
 
-            frente2.Transform = TGCMatrix.Translation(new TGCVector3(0, frente.BoundingBox.calculateBoxRadius(), -arriba.BoundingBox.PMin.Z));
+            frente2.Transform = TGCMatrix.Translation(new TGCVector3(0, 21.27f, -21.45f));
             frente2.BoundingBox.transform(frente2.Transform);
-            arriba2.Transform = TGCMatrix.Translation(new TGCVector3(0, frente.BoundingBox.calculateBoxRadius(), -arriba.BoundingBox.calculateBoxRadius()));
+            arriba2.Transform = TGCMatrix.Translation(new TGCVector3(0, 21.7f, -21.47f));
             arriba2.BoundingBox.transform(arriba2.Transform);
 
             escalones.Add(new Escalon(frente2, arriba2));
+
+            // Tercer escalon
+            var frente3 = frente.createMeshInstance("frente3");
+
+            var arriba3 = arriba.createMeshInstance("arriba3");
+
+            frente3.Transform = TGCMatrix.Translation(new TGCVector3(0, 43.64f, -40.39f));
+            frente3.BoundingBox.transform(frente3.Transform);
+            arriba3.Transform = TGCMatrix.Translation(new TGCVector3(0, 43.76f, -40.32f));
+            arriba3.BoundingBox.transform(arriba3.Transform);
+
+            escalones.Add(new Escalon(frente3, arriba3));
+
+            // Cuarto escalon
+            var frente4 = frente.createMeshInstance("frente4");
+
+            var arriba4 = arriba.createMeshInstance("arriba4");
+
+            frente4.Transform = TGCMatrix.Translation(new TGCVector3(0, 66.62f, -58.94f));
+            frente4.BoundingBox.transform(frente4.Transform);
+            arriba4.Transform = TGCMatrix.Translation(new TGCVector3(0, 66.28f, -58.84f));
+            arriba4.BoundingBox.transform(arriba4.Transform);
+
+            escalones.Add(new Escalon(frente4, arriba4));
+
+            // Cuarto escalon
+            var frente5 = frente.createMeshInstance("frente5");
+
+            var arriba5 = scene.Meshes.Find(mesh => mesh.Name.Contains("ultimo"));
+
+            frente5.Transform = TGCMatrix.Translation(new TGCVector3(0, 88.18f, -77.65f));
+            frente5.BoundingBox.transform(frente5.Transform);
+
+            escalones.Add(new Escalon(frente5, arriba5));
         }
 
         public override void Colisiones()
